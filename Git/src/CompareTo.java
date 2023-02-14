f6bda645491970fdf800522f8d99fb0342b3241d
nandur9gmail <nandur9gmail@ip-172-31-18-55.ec2.internal>
2023-02-14 01:46:10 +0000

    CompareTo

---
+++ Git/src/CompareTo.java
@@ -0,0 +1,22 @@
+
+public class CompareTo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String S1 ="Aello";
+		String S2 ="Aello";
+		String S3 ="Sello";
+		char C1 ='A';
+		String strDouble;
+		  System.out.println("S1 compare S2 is:"+ S1.compareTo(S2));
+		  System.out.println("S1 compare S2 is:"+ S1.compareTo(S3));
+		 // System.out.println("unicode value of A:"+Integer.toString(int(c1));
+		  strDouble = String.format("%.2f", 1.23456);
+		  System.out.println(S1.concat(S3));
+		  System.out.println(strDouble);
+		  
+
+	}
+
+}
