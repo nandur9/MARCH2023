de41f05248f78e23924bf6ede9e76c82ade098d7
nandur9gmail <nandur9gmail@ip-172-31-18-55.ec2.internal>
2023-02-14 02:15:12 +0000

    StringBuffering

---
+++ Git/src/StringBuffer.java
@@ -0,0 +1,26 @@
+
+public class StringBuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer sb = new StringBuffer();
+	    /*System.out.println(str);
+	     System.out.println(str.length());
+	     str.setLength(8);
+	     System.out.println(str);
+         */
+		sb.append("Java");
+		System.out.println(sb);
+		
+	}
+
+	private void append(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	}
+
+
---
+++ Git/src/StringBuilderDemo.java
@@ -0,0 +1,16 @@
+
+public class StringBuilderDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuilder str = new StringBuilder("Welcome to Java");
+		     System.out.println(str);
+		     System.out.println(str.length());
+		     str.setLength(8);
+		     System.out.println(str);
+		
+
+	}
+
+}
